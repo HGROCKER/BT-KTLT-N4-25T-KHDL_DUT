3f8613d3fd8f37fbaaf8aa9fd504840f85751cbe
HGROCKER <135489210+HGROCKER@users.noreply.github.com>
2025-11-16 18:16:04 +0700

    Update Bai37 -TVKH - 25T_KHDL.c

--- Bai37 -TVKH - 25T_KHDL.c
+++ Bai37 -TVKH - 25T_KHDL.c
@@ -1,34 +1,47 @@
 #include <stdio.h>
 #include <math.h>
-#define MAX 2645
-
-int phanTich(int n,int b[], int nho){
-int k = sqrt(n);
-for(int i=0;i<=k;i++)b[i]=0;
-while(n){
-b[k]+=n/(k*k);
-if(b[k]){nho=k;}
-n%=k*k;
-k--;
+void solve(){
+	long long N,sqrtX;
+    printf("Nhap N: ");
+    scanf("%lld", &N);
+	sqrtN = sqrt(N);
+    if (N == sqrtX*sqrtX) {
+        printf("%lld = %lld^2\n", N, sqrtX);
+        return 0;
+    }
+    for (long long a=0;a*a<=N;a++){
+        long long b2=N-a*a;
+		sqrtX = sqrt(b2);
+        if (b2 == sqrtX*sqrtX) {
+            printf("%lld = %lld^2 + %lld^2\n", N, a,sqrtX);
+            return 0;
+        }
+    }
+    for (long long a=0;a*a<=N;a++) {
+        for (long long b=0;b*b<=N-a*a;b++) {
+            long long c2=N-a*a-b*b;
+			sqrtX=sqrt(c2);
+            if (c2 >= 0 && c2==sqrtX*sqrtX) {
+                printf("%lld = %lld^2 + %lld^2 + %lld^2\n",N,a,b,sqrtX);
+                return 0;
+            }
+        }
+    }
+    for (long long a=0;a*a<=N;a++) {
+        for (long long b=0;b*b<=N-a*a;b++) {
+            long long k = N-a*a-b*b;
+            for (long long c=0;c*c<=k;c++) {
+                long long d2=k-c*c;
+				sqrtX=sqrt(d2);
+                if (d2>=0&&d2==sqrtX*sqrtX){
+                    printf("%lld = %lld^2 + %lld^2 + %lld^2 + %lld^2\n",N,a,b,c,sqrtX);
+                    return 0;
+                }
+            }
+        }
+    }
 }
-return nho;
-}
-void OUTPUT(int b[],int k, int nho){
-	for(int i=k;i>nho;i--){
-		for(int j=0;j<b[i];j++){
-			printf("%d^2 + ",i);
-		}
-	}
-	for(int j=0;j<b[nho]-1;j++){
-			printf("%d^2 + ",nho);
-		}printf("%d^2",nho);
-}
-
-int main(){
-	int b[MAX],nho=2645,n;
-	scanf("%d",&n);
-	nho = phanTich(n,b,nho);
-	OUTPUT(b,sqrt(n),nho);
-	
-	return 0;
+int main() {
+    solve();
+    return 0;
 }
