d13a23f70e2089d7a352dc912dc110dadeb3651a
HGROCKER <135489210+HGROCKER@users.noreply.github.com>
2025-11-16 18:49:54 +0700

    Update Bai37 -TVKH - 25T_KHDL.c

--- Bai37 -TVKH - 25T_KHDL.c
+++ Bai37 -TVKH - 25T_KHDL.c
@@ -1,41 +1,42 @@
 #include <stdio.h>
 #include <math.h>
+#define ll long long
 void solve(){
-	long long N,sqrtX;
+	ll N,sqrtX;
     printf("Nhap N: ");
     scanf("%lld", &N);
-	sqrtN = sqrt(N);
+	sqrtX = sqrt(N);
     if (N == sqrtX*sqrtX) {
-        printf("%lld = %lld^2\n", N, sqrtX);
-        return 0;
+        printf("%lld = %lld^2\n",N,sqrtX);
+        return ;
     }
-    for (long long a=0;a*a<=N;a++){
-        long long b2=N-a*a;
+    for (ll a=0;a*a<=N;a++){
+        ll b2=N-a*a;
 		sqrtX = sqrt(b2);
         if (b2 == sqrtX*sqrtX) {
-            printf("%lld = %lld^2 + %lld^2\n", N, a,sqrtX);
-            return 0;
+            printf("%lld = %lld^2 + %lld^2\n",N,sqrtX,a);
+            return ;
         }
     }
-    for (long long a=0;a*a<=N;a++) {
-        for (long long b=0;b*b<=N-a*a;b++) {
-            long long c2=N-a*a-b*b;
+    for (ll a=0;a*a<=N;a++) {
+        for (ll b=0;b*b<=N-a*a;b++) {
+            ll c2=N-a*a-b*b;
 			sqrtX=sqrt(c2);
             if (c2 >= 0 && c2==sqrtX*sqrtX) {
-                printf("%lld = %lld^2 + %lld^2 + %lld^2\n",N,a,b,sqrtX);
-                return 0;
+                printf("%lld = %lld^2 + %lld^2 + %lld^2\n",N,sqrtX,b,a);
+                return ;
             }
         }
     }
-    for (long long a=0;a*a<=N;a++) {
-        for (long long b=0;b*b<=N-a*a;b++) {
-            long long k = N-a*a-b*b;
-            for (long long c=0;c*c<=k;c++) {
-                long long d2=k-c*c;
+    for (ll a=0;a*a<=N;a++) {
+        for (ll b=0;b*b<=N-a*a;b++) {
+            ll k = N-a*a-b*b;
+            for (ll c=0;c*c<=k;c++) {
+                ll d2=k-c*c;
 				sqrtX=sqrt(d2);
                 if (d2>=0&&d2==sqrtX*sqrtX){
-                    printf("%lld = %lld^2 + %lld^2 + %lld^2 + %lld^2\n",N,a,b,c,sqrtX);
-                    return 0;
+                    printf("%lld = %lld^2 + %lld^2 + %lld^2 + %lld^2\n",N,sqrtX,c,b,a);
+                    return ;
                 }
             }
         }
