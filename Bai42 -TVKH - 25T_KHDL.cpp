0b62b576be2429809c2e96c4737ed180145f4347
HGROCKER <135489210+HGROCKER@users.noreply.github.com>
2025-11-16 17:27:47 +0700

    Update Bai42 -TVKH - 25T_KHDL.cpp

--- Bai42 -TVKH - 25T_KHDL.cpp
+++ Bai42 -TVKH - 25T_KHDL.cpp
@@ -1,10 +1,14 @@
 #include<stdio.h>
-int main(){
+void solve(){
     long long a=0,s=0;
     do{
         s+=a;
         scanf("%lld",&a);
     }while(a);
     printf("Tong cac so da nhap: %lld\n",s);
+}
+
+int main(){
+    solve();
     return 0;
 }
